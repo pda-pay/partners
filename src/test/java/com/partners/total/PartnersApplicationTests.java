a7c6a851f29355ab5a95fbf4c260fc1cef31576f
YaRkyungmin <kkgm94@naver.com>
2024-08-23 11:20:12 +0900

    ✨ feat: test에 PartnersApplicationTests 추가

---
+++ src/test/java/com/partners/total/PartnersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partners.total;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartnersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
