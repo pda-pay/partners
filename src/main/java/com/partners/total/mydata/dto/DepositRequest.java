078371551e17e1644488480eea3ce20bf1a176b0
YaRkyungmin <kkgm94@naver.com>
2024-08-26 17:50:58 +0900

    ✨ feat: Deposit Dto 정의

---
+++ src/main/java/com/partners/total/mydata/dto/DepositRequest.java
@@ -0,0 +1,9 @@
+package com.partners.total.mydata.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+public class DepositRequest {
+    private int accountId;
+}
---
+++ src/main/java/com/partners/total/mydata/dto/DepositResponse.java
@@ -0,0 +1,10 @@
+package com.partners.total.mydata.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class DepositResponse {
+    private final int deposit;
+}
