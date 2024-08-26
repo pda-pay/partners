903fbb70ce42cdb4ca031f46b588c99497d34234
YaRkyungmin <kkgm94@naver.com>
2024-08-26 17:48:25 +0900

    ✨ feat: exception 및 handler 정의

---
+++ src/main/java/com/partners/total/mydata/exception/AccountIdNotFoundException.java
@@ -0,0 +1,7 @@
+package com.partners.total.mydata.exception;
+
+public class AccountIdNotFoundException extends RuntimeException {
+    public AccountIdNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/partners/total/mydata/exception/CalculateAccountError.java
@@ -0,0 +1,7 @@
+package com.partners.total.mydata.exception;
+
+public class CalculateAccountError extends RuntimeException {
+    public CalculateAccountError(String message) {
+        super(message);
+    }
+}
