9a80a90a888ce8719414a9c67e2c9adf7f3ca8c3
jody816 <81346079+jody816@users.noreply.github.com>
2024-08-27 13:40:47 +0900

    ⚡️ update: StocksRepository 관련 로직 수정
    불필요 코드 제거

--- src/main/java/com/partners/total/mydata/domain/StocksRepository.java
+++ src/main/java/com/partners/total/mydata/domain/StocksRepository.java
@@ -1,9 +1,6 @@
 package com.partners.total.mydata.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,11 +9,4 @@ public interface StocksRepository extends JpaRepository<Stocks, Integer> {
     List<Stocks> findByAccount(Account account);
 
     Optional<Stocks> findStocksByAccountIdAndStockCode(int accountId, String stockCode);
-
-    @Modifying
-    @Query("UPDATE Stocks s SET s.quantity = :quantity WHERE s.id = :id")
-    Optional<Integer> updateQuantityByAccountIdAndQuantity(@Param("id") Integer id, @Param("quantity") Integer quantity);
-
-//    @Query("SELECT Stocks FROM Stocks WHERE account.id = :accountId")
-    List<Stocks> findStocksByAccountId(int accountId);
 }
