1a97045ff2de43d297d6c4b4d0f9485b219126ed
jody816 <81346079+jody816@users.noreply.github.com>
2024-08-27 10:27:39 +0900

    ✨ feat: StockPriorityDTO 스웨거 적용
    스키마 어노테이션을 통해 스웨거 테스트 적용

--- src/main/java/com/partners/total/securities/dto/StockPriorityDTO.java
+++ src/main/java/com/partners/total/securities/dto/StockPriorityDTO.java
@@ -1,5 +1,6 @@
 package com.partners.total.securities.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +8,12 @@ import lombok.Setter;
 @Setter
 public class StockPriorityDTO {
 
+    @Schema(description = "계좌 id", example = "2")
     private int accountId;
+
+    @Schema(description = "증권 수량", example = "1")
     private int quantity;
+
+    @Schema(description = "주식 코드", example = "005930")
     private String stockCode;
 }
